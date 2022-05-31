9ce5556e2fe46fbc279534d68689f27a65c78657
Skii <106560997+MaskedSkii@users.noreply.github.com>
2022-05-31 18:14:38 +0100

    Create emoji chatter.lua

---
+++ emoji chatter.lua
@@ -0,0 +1,325 @@
+game:GetService("Players").LocalPlayer.Chatted:Connect(function(msg)
+if msg:lower() == "/e rofl" then
+
+local args = {
+    [1] = "🤣",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+
+else if msg:lower() == "/e laugh" then
+    
+    local args = {
+    [1] = "😂",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+
+    else if msg:lower() == "/e smile" then
+        
+        local args = {
+            [1] = "😀",
+            [2] = "All"
+        }
+        
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+       
+        else if msg:lower() == "/e xd" then
+            local args = {
+                [1] = "😆",
+                [2] = "All"
+              }
+               
+                
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+            
+            
+            else if msg:lower() == "/e angel" then
+                local args = {
+                    [1] = "😇",
+                    [2] = "All"
+                }
+           
+           game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+           
+           else if msg:lower() == "/e wink" then
+                local args = {
+                    [1] = "😉",
+                    [2] = "All"
+                }
+                game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+                
+               else if msg:lower() == "/e yum" then
+                   local args = {
+                       [1] = "😋 ",
+                       [2] = "All"
+                   }
+                   
+                   game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+               
+               else if msg:lower() == "/e nerd" then
+                    local args = {
+                        [1] = "🤓",
+                        [2] = "All"
+                    }
+                    
+                    game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+               
+                      else if msg:lower() == "/e sunglasses" or "/e cool" then
+                          local args = {
+                              [1] = "😎",
+                              [2] = "All"
+                          }
+                          
+                          game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+                          
+                          else if msg:lower() == "/e smirk" then
+                              local args = {
+                                  [1] = "😏",
+                                  [2] = "All"
+                              }
+                          
+                          
+                          game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+                          
+                          else if msg:lower() == "/e depressed" then
+                             local args = {
+                                 [1] = "😔",
+                                 [2] = "All"
+                             }
+                          
+                          game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+                          
+                          else if msg:lower() == "/e sad" then
+                              local args = {
+                                  [1] = "☹️",
+                                  [2] = "All"
+                              }
+                          
+                          game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+                          
+                          else if msg:lower() == "/e weary" then
+                              local args = {
+                                  [1] = "😩",
+                                  [2] = "All"
+                              }
+                          
+                          game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+                          
+                          else if msg:lower() == "/e cry" then
+                              local args = {
+                                  [1] = "😢",
+                                  [2] = "All"
+                              }
+                              
+                          game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+                          
+                          
+                          else if msg:lower() == "/e sob" then
+                              local args = {
+                                  [1] = "😭",
+                                  [2] = "All"
+                              }
+                          
+                          game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+    
+                        else if msg:lower() == "/e angry" then
+                            local args = {
+                                [1] = "😡",
+                                [2] = "All"
+                            }
+                            
+                        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+                        
+                        else if msg:lower() == "/e flushed" then
+                            local args = {
+                                [1] = "😳", 
+                                [2] = "All"
+                            }
+    
+                        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+                          
+                          else if msg:lower() == "/e hot" then
+                              local args = {
+                                  [1] = "🥵",
+                                  [2] = "All"
+                              }
+                          
+                          game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+                          
+                          else if msg:lower() == "/e cold" then
+                              local args = {
+                                  [1] = "🥶",
+                                  [2] = "All"
+                              }
+                              
+                          game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+                          
+                          else if msg:lower() == "/e scared" then
+                              local args = {
+                                  [1] = "😱",
+                                  [2] = "All"
+                              }
+                          
+                          game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+                          
+                          else if msg:lower() == "/e neutral" then 
+                              local args = {
+                                  [1] = "😐",
+                                  [2] = "All"
+                              }
+                          
+                          game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+                          
+                          else if msg:lower() == "/e shocked" then
+                              local args = {
+                                  [1] = "😮",
+                                  [2] = "All"
+                              }
+                          
+                          game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+                          
+                          else if msg:lower() == "/e yawn" then
+                              local args = {
+                                  [1] = "🥱"
+                                  [2] = "All"
+                              }
+                          
+                          game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+                          
+                          else if msg:lower() == "/e sleepy" then
+                              local args = {
+                                  [1] = "😴",
+                                  [2] = "All"
+                              }
+                          
+                          game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+                          
+                          else if msg:lower() == "/e drool" then
+                              local args = {
+                                  [1] = "🤤",
+                                  [2] = "All"
+                              }
+                          
+                          game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+                          
+                          else if msg:lower() == "/e vomit" then
+                              local args = {
+                                  [1] = "🤮",
+                                  [2] = "All"
+                              }
+                          
+                          game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+                          
+                          
+                          else if msg:lower() == "/e money_eyes" then
+                              local args = {
+                                  [1] = "🤑",
+                                  [2] = "All"
+                              }
+                          
+                          game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+                          
+                          else if msg:lower() == "/e cowboy" then
+                              local args = {
+                                  [1] = "🤠",
+                                  [2] = "All"
+                              }
+                              
+                          game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+                          
+                          else if msg:lower() == "/e clown" then
+                              local args = {
+                                  [1] = "🤡",
+                                  [2] = "All"
+                              }
+                              
+                          game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+                          
+                          else if msg:lower() == "/e ghost" then
+                              local args = {
+                                  [1] = "👻",
+                                  [2] = "All"
+                              }              
+                          
+                          game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+                          
+                          else if msg:lower() == "/e skull" then
+                              local args = {
+                                  [1] = "💀",
+                                  [2] = "All"
+                              }
+                          
+                          game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+                          
+                          else if msg:lower() == "/e alien" then
+                              local args = {
+                                  [1] = "👽",
+                                  [2] = "All"
+                              }
+                          
+                          game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+                          
+                          else if msg:lower() == "/e video_game_monster" then
+                              local args = {
+                                  [1] = "👾",
+                                  [2] = "All"
+                              }
+                          
+                          game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+                          
+                          else if msg:lower() == "/e robot" then
+                              local args = {
+                                  [1] = "🤖",
+                                  [2] = "All"
+                              }
+                          
+                          game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+                          
+                          else if msg:lower() == "/e pumpkin" then
+                              local args = {
+                                  [1] = "🎃",
+                                  [2] = "All"
+                              }
+                          
+                          game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+
+                                                                    end
+                                                                  end
+                                                                end
+                                                              end
+                                                            end
+                                                          end 
+                                                        end
+                                                      end
+                                                    end 
+                                                  end
+                                                end
+                                              end
+                                            end
+                                          end
+                                        end
+                                      end
+                                    end
+                                  end
+                                end
+                              end
+                            end
+                          end
+                        end
+                       end
+                     end
+                   end
+                 end
+               end
+            end
+          end
+        end
+      end 
+    end
+  end
+end
+
+end)
